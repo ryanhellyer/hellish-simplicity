ac48844709600d4e88f61acdca40924141ee0a9c
Ryan <ryanhellyer@gmail.com>
2021-05-09 22:02:29 +0200

    Addition of initial index system for FuseJS

--- functions.php
+++ functions.php
@@ -1,244 +1,35 @@
 <?php
 
 /**
- * Primary class used to load the Hellish Simplicity theme.
+ * Autoload the classes.
+ * Includes the classes, and automatically instantiates them via spl_autoload_register().
  *
- * @copyright Copyright (c), Ryan Hellyer
- * @license http://www.gnu.org/licenses/gpl.html GPL
- * @author Ryan Hellyer <ryanhellyer@gmail.com>
- * @package Hellish Simplicity
- * @since Hellish Simplicity 1.5
+ * @param string $class The class being instantiated.
  */
-class Hellish_Simplicity_Setup {
+function autoload_hellish_simplicity_support( $class ) {
 
-	/**
-	 * Theme version number.
-	 * 
-	 * @var string
-	 */
-	const VERSION_NUMBER = '2.1';
-
-	/**
-	 * The default header text.
-	 * 
-	 * @var string
-	 */
-	const DEFAULT_HEADER_TEXT = 'Custom<span>Header</span><small>.com</small>';
-
-	/**
-	 * The header text option name.
-	 * 
-	 * @var string
-	 */
-	const HEADER_TEXT_OPTION = 'header-text';
-
-	/**
-	 * Theme name.
-	 * 
-	 * @var string
-	 */
-	const THEME_NAME = 'hellish-simplicity';
-
-	/**
-	 * Constructor.
-	 * Add methods to appropriate hooks and filters.
-	 *
-	 * @global  int  $content_width  Sets the media widths (unfortunately required as a global due to WordPress core requirements).
-	 */
-	public function __construct() {
-		global $content_width;
-		$content_width = 680;
-
-		// Add action hooks.
-		add_action( 'admin_init', array( $this, 'add_option' ) );
-		add_action( 'after_setup_theme', array( $this, 'theme_setup' ) );
-		add_action( 'widgets_init', array( $this, 'widgets_init' ) );
-		add_action( 'wp_enqueue_scripts', array( $this, 'stylesheet' ) );
-		add_action( 'admin_init', array( $this, 'editor_stylesheet' ) );
-		add_action( 'wp_enqueue_scripts', array( $this, 'comment_reply' ) );
-		add_action( 'customize_register', array( $this, 'customize_register' ) );
-		add_action( 'customize_render_control_' . self::HEADER_TEXT_OPTION,  array( $this, 'customizer_help' ) );
-		add_action( 'admin_head', array( $this, 'admin_menu_link' ) );
-
-		// Add filters.
-		add_filter( 'post_class', array( $this, 'add_last_post_class' ) );
+	// Bail out if not loading a Media Manager class,
+	$string = 'Hellish_Simplicity_';
+	if ( $string != substr( $class, 0, strlen( $string ) ) ) {
+		return;
 	}
 
-	/**
-	 * Add the header text option.
-	 */
-	public function add_option() {
-		add_option(
-			self::HEADER_TEXT_OPTION, // The header text option.
-			self::DEFAULT_HEADER_TEXT // The default header text.
-		);
-	}
+	// Convert from the class name, to the classes file name,
+	$file_data = strtolower( $class );
+	$file_data = str_replace( '_', '-', $file_data );
+	$file_name = 'class-' . $file_data . '.php';
 
-	/**
-	 * Comment reply script.
-	 */
-	public function comment_reply() {
+	// Get the classes file path
+	$dir = dirname( __FILE__ );
+	$path = $dir . '/inc/' . $file_name;
 
-		// Bail out now if in admin panel.
-		if ( is_admin() ) {
-			return;
-		}
-
-		// HTML 5 shiv script.
-		wp_enqueue_script( self::THEME_NAME . '-html5-shiv', get_template_directory_uri() . '/js/html5-shiv.min.js', null, SELF::VERSION_NUMBER );
-		wp_script_add_data( self::THEME_NAME . '-html5-shiv', 'conditional', 'lt IE 9' );
-
-		// Comment reply script.
-		if ( is_singular() ) {
-			wp_enqueue_script( 'comment-reply' );
-		}
-	}
-
-	/**
-	 * Load editor stylesheet.
-	 */
-	public function editor_stylesheet() {
-		add_editor_style( 'css/editor-style.css' );
-	}
-
-	/**
-	 * Load stylesheet.
-	 */
-	public function stylesheet() {
-		if ( ! is_admin() ) {
-			wp_enqueue_style( self::THEME_NAME, get_stylesheet_directory_uri() . '/css/style.min.css', array(), self::VERSION_NUMBER );
-		}
-	}
-
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 */
-	public function theme_setup() {
-
-		// Make theme available for translation.
-		load_theme_textdomain( 'hellish-simplicity', get_template_directory() . '/languages' );
-
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
-
-		// Add title tags.
-		add_theme_support( 'title-tag' );
-
-		// Enable support for Post Thumbnails.
-		add_theme_support( 'post-thumbnails' );
-		add_image_size( self::THEME_NAME . '-excerpt-thumb', 250, 350 );
-		add_image_size( self::THEME_NAME . '-attachment-page', 1000, 1500 );
-	}
-
-	/**
-	 * Register widgetized area and update sidebar with default widgets.
-	 */
-	public function widgets_init() {
-		register_sidebar(
-			array(
-				'name'          => esc_html__( 'Sidebar', 'hellish-simplicity' ),
-				'id'            => 'sidebar',
-				'before_widget' => '<aside id="%1$s" class="%2$s">',
-				'after_widget'  => '</aside>',
-				'before_title'  => '<h2 class="widget-title">',
-				'after_title'   => '</h2>',
-			)
-		);
-	}
-
-	/**
-	 * Implements Page Styler theme options into Theme Customizer.
-	 *
-	 * @param object $wp_customize Theme Customizer object.
-	 */
-	public function customize_register( $wp_customize ) {
-
-		// Theme Footer.
-		$wp_customize->add_setting( self::HEADER_TEXT_OPTION, array(
-			'type'              => 'option',
-			'sanitize_callback' => array( $this, 'sanitize' ),
-			'capability'        => 'edit_theme_options',
-		) );
-		$wp_customize->add_section( 'header_text', array(
-			'title'             => esc_html__( 'Header Text', 'hellish-simplicity' ),
-			'priority'          => 10,
-		) );
-		$wp_customize->add_control( self::HEADER_TEXT_OPTION, array(
-			'section'           => 'header_text',
-			'label'             => esc_html__( 'Header Text', 'hellish-simplicity' ),
-			'type'              => 'text',
-		) );
-
-	}
-
-	/**
-	 * Adding extra helpful information to the customizer.
-	 */
-	public function customizer_help() {
-		echo '
-		<li>
-			<p>
-				' . esc_html__( 'Example text:', 'hellish-simplicity' ) . ' <code>' . esc_html( self::DEFAULT_HEADER_TEXT ) . '</code>
-			</p>
-		</li>';
-	}
-
-	/**
-	 * Adds a class of .last-post to the last post in a loop.
-	 * This method is discussed here https://geek.hellyer.kiwi/tools/add-class-to-last-post-in-loop/
-	 * 
-	 * @param array $classes The array of post classes.
-	 * @return array The array of post classes, with .last-post added.
-	 */
-	public function add_last_post_class( $classes ) {
-		global $wp_query;
-
-		if ($wp_query->current_post == ( $wp_query->post_count - 1 ) ) {
-			$classes[] = 'last-post';
-		}
-
-		return $classes;
-	}
-
-	/**
-	 * Adds an admin menu link to the header section of the customizer.
-	 * This is required because this theme does not use a graphical header image.
-	 * Standard graphical custom header images automatically add this.
-	 *
-	 * @global array $submenu.
-	 */
-	public function admin_menu_link() {
-		global $submenu;
-
-		// Only display header admin menu link when in admin panel and when user is allowed to edit theme options.
-		if ( ! is_admin() && ! current_user_can( 'edit_theme_options' ) ) {
-			return;
-		}
-
-		$themes_submenu[0] = array(
-			0 => esc_html__( 'Header', 'hellish-simplicity' ),
-			1 => 'edit_theme_options',
-			2 => 'customize.php?autofocus%5Bcontrol%5D=' . self::HEADER_TEXT_OPTION,
-		);
-
-		// Merging menus together
-		$submenu['themes.php'] = array_merge( $submenu['themes.php'], $themes_submenu );
-	}
-
-	/**
-	 * Sanitizing the header text.
-	 *
-	 * @param string $header_text  The header text.
-	 * @return string The sanitized header text.
-	 * @access static
-	 */
-	static public function sanitize( $header_text ) {
-		$allowed_html = array(
-			'small' => array(),
-			'span' => array(),
-		);
-		return wp_kses( $header_text, $allowed_html );
-	}
-
+	// Include the class (spl_autoload_register will automatically instantiate it for us),
+	require( $path );
 }
+spl_autoload_register( 'autoload_hellish_simplicity_support' );
+
+/**
+ * Instantiate classes.
+ */
 new Hellish_Simplicity_Setup;
+new Hellish_Simplicity_Index;
---
+++ inc/class-hellish-simplicity-index.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Indexes the posts for use by FuseJS.
+ *
+ * @copyright Copyright (c), Ryan Hellyer
+ * @license http://www.gnu.org/licenses/gpl.html GPL
+ * @author Ryan Hellyer <ryanhellyer@gmail.com>
+ * @package Hellish Simplicity
+ * @since Hellish Simplicity 2.2
+ */
+class Hellish_Simplicity_Index {
+
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+
+		if ( '/hellish-simplicity-index/' === $_SERVER['REQUEST_URI'] ) {
+			$this->index_posts();
+		}
+	}
+
+
+	/**
+	 * Grabs list of all posts.
+	 *
+	 * @param bool $search Whether to return search data or not.
+	 * @global object $wpdb The WordPress database object.
+	 */
+	public function index_posts() {
+		global $wpdb;
+
+		$post_types           = array( 'post', 'page' );
+		$post_type_string     = "'" . implode( "','", $post_types ) . "'";
+		$post_statuses        = array( 'publish' );
+		$post_statuses_string = "'" . implode( "','", $post_statuses ) . "'";
+		$posts_table_name     = "{$wpdb->prefix}posts";
+		$rel_table_name       = "{$wpdb->prefix}term_relationships";
+		$term_tax_table_name  = "{$wpdb->prefix}term_taxonomy";
+		$taxonomies           = get_taxonomies( array( 'public' => true ) );
+		$taxonomies_string    = '("' . implode( '","', $taxonomies ) . '")';
+// QUERY SHOULD BE SIMPLIFIED
+		$query_string =
+			'SELECT p.*, GROUP_CONCAT(tt.term_id) as term_ids FROM `' . $posts_table_name . '` AS p '
+			. 'LEFT JOIN `' . $rel_table_name . '` tr ON tr.object_id = p.id '
+			. 'LEFT JOIN `' . $term_tax_table_name . '` tt ON tt.term_taxonomy_id = tr.term_taxonomy_id AND tt.taxonomy IN '
+			. $taxonomies_string . ' '
+			. ' WHERE post_status IN ( ' . $post_statuses_string . ' ) 
+			AND post_type IN ( ' . implode(',', array_fill(0, count( $post_types ), '%s') ) . ' ) '
+			. 'GROUP BY p.id';
+
+		$query = $wpdb->prepare( $query_string, $post_types );
+
+		$posts = $wpdb->get_results( $query );
+
+		$index = array();
+		foreach ( $posts as $key => $post ) {
+			$index[] = array(
+				'id'                 => absint( $post->ID ),
+				'author_id'          => absint( $post->post_author ),
+				'timestamp'          => strtotime( $post->post_date_gmt ),
+				'content'            => wp_kses_post( $post->post_content ),
+				'post_title'         => esc_html( $post->post_title ),
+				'excerpt'            => wp_kses_post( $post->post_excerpt ),
+				'slug'               => esc_attr( $post->post_name ),
+				'modified_timestamp' => strtotime( $post->post_modified_gmt ),
+				'term_ids'           => (array) $post->term_ids,
+			);
+		}
+
+
+		echo json_encode( $index );
+		die;
+	}
+
+}
